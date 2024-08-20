7c8f56bfbe4eece2f28768aed134fa2a339ef833
Maiilaa <zirarimaila8@gmail.com>
2024-08-20 12:00:20 +0200

    12H

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV Zirari Maïla</title>
+    <link href="Assets/css/cv.css" rel="stylesheet" />
 </head>
 <body>
     <header>
-    <h1>YES SIIIIR</h1>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Etudes</li>
+                <li>Experiences Pro</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
     </header>
     <main>
 
