f576e49d98ca3c8f160cd1e96611d91ced76d3eb
Maiilaa <zirarimaila8@gmail.com>
2024-08-21 11:59:47 +0200

    12h

--- index.php
+++ index.php
@@ -4,98 +4,125 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV Zirari Maïla</title>
-    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet"  href="Assets/css/cv.css?t=<?=time()?>"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="Assets/css/cv.css?t=<?=time()?>"/>
+    <style>
+        .icon-plesk {
+            width: 24px;
+            height: 24px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #007bff;
+            color: white;
+            border-radius: 50%;
+            font-size: 16px;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-    <header>
-       
+    <header class="text-center py-4 bg-light">
+        <h1>MAÏLA ZIRARI</h1>
     </header>
     <main>
-    <h1>ZIRARI MAÏLA</h1>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="bg-light p-3 rounded shadow-sm">
-                    <p><strong>Téléphone :</strong> 07 61 82 59 78</p>
-                    <p><strong>Email :</strong> zirarimaila8@gmail.com</p>
-                    <p><strong>Adresse :</strong> 44 bd Charles Mattei, 13014 Marseille</p>
-                    <p><strong>Permis :</strong> B</p>
-                    <a href ="https://github.com/Maiilaa" class="github"><strong>Mon GitHub</strong></a><br/>
-                    <a href ="https://maila-zirari.students-laplateforme.io" class=""><strong>Mon Plesk</strong></a>
-                </div>
-                <div class="bg-light p-3 mt-4 rounded shadow-sm">
-                    <h4>Compétences</h4>
-                    <ul>
-                        <li>Résolution de problèmes complexes</li>
-                        <li>Esprit d’équipe</li>
-                        <li>Esprit d’analyse</li>
-                        <li>Curieuse</li>
-                        <li>Rigoureuse</li>
-                        <li>Autonome</li>
-                    </ul>
-                </div>
-                <div class="bg-light p-3 mt-4 rounded shadow-sm">
-                    <h5>Langues</h5>
-                    <ul>
-                        <li>Français</li>
-                        <li>Anglais</li>
-                        <li>Allemand</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-md-8">
-                <div class="bg-light p-4 rounded shadow-sm">
-                    <h2>Expériences Professionnelles</h2>
-                    <div class="mt-4">
-                        <h6><strong>L'Atelier</strong></h6>
-                        <p><strong>Fonctions :</strong></p>
-                        <ul>
-                            <li>réalisation de site web en projet collectif</li>
-                            <li>gestion de projet</li>
-                            <li>mise en place et gestion de base de donnée</li>
-                        </ul>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="bg-light p-3 rounded shadow-sm">
+                        <p><strong><i class="bi bi-telephone-fill"></i> </strong> 07 61 82 59 78</p>
+                        <p><strong><i class="bi bi-envelope-fill"></i></strong> zirarimaila8@gmail.com</p>
+                        <p><strong><i class="bi bi-geo-alt-fill"></i> </strong> Marseille</p>
+                        <p><strong><i class="bi bi-car-front-fill"></i></strong> Permis B</p>
+                        <a href="https://github.com/Maiilaa" class="github">
+                            <p><strong><i class="bi bi-github"></i></strong> Mon Github</p>
+                        </a>
+                        <!-- Icône de Plesk remplacée par la lettre P -->
+                        <a href="https://maila-zirari.students-laplateforme.io" class="">
+                            <p><strong><span class="icon-plesk">P</span></strong> Mon Plesk</p>
+                        </a>
                     </div>
-                    <div class="mt-4">
-                        <strong>My Zone de Talent</strong>
-                        <p><strong>Réalisations :</strong></p>
-                        <ul>
-                            <li>Réalisation d’un site web en projet collectif</li>
-                            <li>Réalisation de maquette UX/UI</li>
-                            <li>Codage HTML/CSS</li>
-                            <li>Analyse et interprétation des données</li>
-                        </ul>
+                    <div class="bg-light p-3 mt-4 rounded shadow-sm">
+                        <h4>Compétences</h4>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-html" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">HTML</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-css" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">CSS</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-php" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">PHP</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-github" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">GitHub</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-figma" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">Figma</div>
+                        </div>
                     </div>
-                    <div class="mt-4">
-                        <strong>BNP Paribas</strong>
-                        <p><strong>Fonctions :</strong></p>
-                        <ul>
-                            <li>Accueil de la clientèle</li>
-                            <li>Enregistrer des moyens de paiement</li>
-                            <li>Proposition de services bancaires</li>
-                            <li>Gestion de dossiers</li>
-                        </ul>
+                    <div class="bg-light p-3 mt-4 rounded shadow-sm">
+                        <h5>Langues</h5>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-français" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">Français</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-anglais" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">Anglais</div>
+                        </div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar progress-bar-allemand" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">Allemand</div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="bg-light p-4 mt-4 rounded shadow-sm">
-                    <h3>Formations</h3>
-                    <ul>
-                        <li>Titre pro développpeur web et web mobile</li>
-                        <li>Licence Maths Info Éco</li>
-                        <li>Baccalauréat Économique et Social Spécialité Mathématiques</li>
-            
-
-                    </ul>
-
+                <div class="col-md-8">
+                    <div class="bg-light p-4 rounded shadow-sm">
+                        <h2>Expériences Professionnelles</h2>
+                        <div class="mt-4">
+                            <h6><strong>L'Atelier</strong></h6>
+                            <p><strong>Fonctions :</strong></p>
+                            <ul>
+                                <li>réalisation de site web en projet collectif</li>
+                                <li>gestion de projet</li>
+                                <li>mise en place et gestion de base de données</li>
+                            </ul>
+                        </div>
+                        <div class="mt-4">
+                            <strong>My Zone de Talent</strong>
+                            <p><strong>Réalisations :</strong></p>
+                            <ul>
+                                <li>Réalisation d’un site web en projet collectif</li>
+                                <li>Réalisation de maquette UX/UI</li>
+                                <li>Codage HTML/CSS</li>
+                                <li>Analyse et interprétation des données</li>
+                            </ul>
+                        </div>
+                        <div class="mt-4">
+                            <strong>BNP Paribas</strong>
+                            <p><strong>Fonctions :</strong></p>
+                            <ul>
+                                <li>Accueil de la clientèle</li>
+                                <li>Enregistrement des moyens de paiement</li>
+                                <li>Proposition de services bancaires</li>
+                                <li>Gestion de dossiers</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="bg-light p-4 mt-4 rounded shadow-sm">
+                        <h3>Formations</h3>
+                        <ul>
+                            <li>Titre pro développeur web et web mobile</li>
+                            <li>Licence Maths Info Éco</li>
+                            <li>Baccalauréat Économique et Social Spécialité Mathématiques</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <footer>
-        
+       
     </footer>
 </body>
 </html>
+>
