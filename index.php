ad37cc87bbbf4cf91a3c09e2a4e6251205826b75
Maiilaa <zirarimaila8@gmail.com>
2024-08-21 10:00:12 +0200

    10h

--- index.php
+++ index.php
@@ -4,23 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CV Zirari Maïla</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="Assets/css/cv.css" rel="stylesheet" />
+    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet"  href="Assets/css/cv.css?t=<?=time()?>"/>
 </head>
 <body>
     <header>
        
     </header>
     <main>
+    <h1>ZIRARI MAÏLA</h1>
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-4">
                 <div class="bg-light p-3 rounded shadow-sm">
-                    <h1>ZIRARI MAÏLA</h1>
                     <p><strong>Téléphone :</strong> 07 61 82 59 78</p>
                     <p><strong>Email :</strong> zirarimaila8@gmail.com</p>
                     <p><strong>Adresse :</strong> 44 bd Charles Mattei, 13014 Marseille</p>
                     <p><strong>Permis :</strong> B</p>
+                    <a href ="https://github.com/Maiilaa" class="github"><strong>Mon GitHub</strong></a><br/>
+                    <a href ="https://maila-zirari.students-laplateforme.io" class=""><strong>Mon Plesk</strong></a>
                 </div>
                 <div class="bg-light p-3 mt-4 rounded shadow-sm">
                     <h4>Compétences</h4>
@@ -47,7 +49,16 @@
                 <div class="bg-light p-4 rounded shadow-sm">
                     <h2>Expériences Professionnelles</h2>
                     <div class="mt-4">
-                    <h6><strong>My Zone de Talent</strong></h6>
+                        <h6><strong>L'Atelier</strong></h6>
+                        <p><strong>Fonctions :</strong></p>
+                        <ul>
+                            <li>réalisation de site web en projet collectif</li>
+                            <li>gestion de projet</li>
+                            <li>mise en place et gestion de base de donnée</li>
+                        </ul>
+                    </div>
+                    <div class="mt-4">
+                        <strong>My Zone de Talent</strong>
                         <p><strong>Réalisations :</strong></p>
                         <ul>
                             <li>Réalisation d’un site web en projet collectif</li>
@@ -71,12 +82,13 @@
                 <div class="bg-light p-4 mt-4 rounded shadow-sm">
                     <h3>Formations</h3>
                     <ul>
-                    <li></li>
-                    
+                        <li>Titre pro développpeur web et web mobile</li>
                         <li>Licence Maths Info Éco</li>
                         <li>Baccalauréat Économique et Social Spécialité Mathématiques</li>
-                        <li></li>
+            
+
                     </ul>
+
                 </div>
             </div>
         </div>
